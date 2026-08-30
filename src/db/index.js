2774c71dcf952ed6823bdb456cf2612fdd043417
kumar siddharth <kumar.siddharth@signzy.com>
2026-08-30 18:30:41 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+// write code in db folder and import it in index file which the code will be clean, separated and modular
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB=async () => {
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MONGODB connected !! DB Host ${connectionInstance.connection.host}`)
+    }
+    catch (error) {
+        console.log("MONGODB connection FAILED", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB
